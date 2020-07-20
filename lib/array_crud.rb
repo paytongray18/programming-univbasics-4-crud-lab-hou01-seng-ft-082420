e7c6c24e7f7c4ffef4cff5e5ce66e86eb2a76bec
Payton Gray <paytongray18@gmail.com>
2020-07-20 14:29:01 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,48 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  sports = ['basketball','football','soccer','baseball']
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  sports = ['basketball','football','soccer','baseball']
+  sports << 'arrays!'
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  sports = ['basketball','football','soccer','baseball']
+  sports.unshift('wow')
 end
 
 def remove_element_from_end_of_array(array)
-  
+  sports = ['wow','basketball','football','soccer','baseball','arrays!']
+  sports.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  sports = ['wow','basketball','football','soccer','baseball']
+  sports.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  sports = ['wow','basketball','am','football','soccer','baseball']
+  sports[2]
 end
 
+
 def retrieve_first_element_from_array(array)
-  
+   sports = ['wow','basketball','am','football','soccer','baseball']
+   sports[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+ sports = ['wow','basketball','football','soccer','baseball','arrays!']
+ sports[5]
 end
 
 def update_element_from_index(array, index_number, element)
-
+ sports = ['wow','basketball','football','soccer','baseball','arrays!']
+ sports[4] = 'totally'
 end
