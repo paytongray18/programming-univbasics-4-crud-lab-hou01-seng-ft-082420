00e06b23ecdb4d354ac71a220e043e4afbf32a6a
Payton Gray <paytongray18@gmail.com>
2020-07-20 14:18:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   sports = ['basketball','football','soccer','baseball']
-  sports << 'hockey'
+  sports << 'arrays!'
 end
 
 def add_element_to_start_of_array(array, element)
