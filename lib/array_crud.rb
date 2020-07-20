c957a5f59d005303ce84af2096f56305ecbfecee
Payton Gray <paytongray18@gmail.com>
2020-07-20 14:21:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  sports = ['wow','basketball','football','soccer','baseball','arrays!']
+  sports.pop
 end
 
 def remove_element_from_start_of_array(array)
