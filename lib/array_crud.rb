1906d819bdc09e96959f7345ca3b63ec44b5a6a7
Payton Gray <paytongray18@gmail.com>
2020-07-20 14:28:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,5 +43,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+ sports = ['wow','basketball','football','soccer','baseball','arrays!']
+ sports[4] = 'totally'
 end
