62e8ec9ed7e40ba0a95a831915b8c8f6c6840ff5
Payton Gray <paytongray18@gmail.com>
2020-07-20 14:13:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  sports = [basketball,football,soccer,baseball]
+  sports = ['basketball','football','soccer','baseball']
 end
 
 def add_element_to_end_of_array(array, element)
