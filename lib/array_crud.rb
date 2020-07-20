dc9ce517a2379a85a69cb850b5c2d658bd5c2334
Payton Gray <paytongray18@gmail.com>
2020-07-20 14:27:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,7 +38,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+ sports = ['wow','basketball','football','soccer','baseball','arrays!']
+ sports[5]
 end
 
 def update_element_from_index(array, index_number, element)
